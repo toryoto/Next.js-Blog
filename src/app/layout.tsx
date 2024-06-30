7ddea955af5e633cbc18f6661297dbb94749c987
toryoto <s1f102002345@iniad.org>
2024-06-30 15:55:27 +0900

    ヘッダーとフッターの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <head />
+      <body className="container mx-auto bg-slate-700 text-slate-50">
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+          </main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
