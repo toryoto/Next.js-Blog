0e4d0bce520261a6ad0ad6e0bfcbc7cec36689b9
toryoto <s1f102002345@iniad.org>
2024-07-02 21:50:30 +0900

    ブログ詳細取得APIの作成

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,17 +1,35 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 // 全データの取得は更新頻度が高いので、SSRを使用
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, {
-    cache: "no-store"
+    cache: "no-store" // SSR
   });
 
   if (!res.ok) {
     throw new Error("エラーが発生しました");
   }
 
-  await new Promise((resolve) => setTimeout(resolve, 1500));
+  await new Promise((resolve) => setTimeout(resolve, 500));
 
   const articles = await res.json();
   return articles;
+}
+
+export const getDetailArticle = async (id: string): Promise<Article[]> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, { next: { revalidate: 60 } }); // ISR
+
+  if (res.status === 400) {
+    notFound()
+  }
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  const article = await res.json();
+  return article;
 }
