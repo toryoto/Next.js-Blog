a3cb25252e6b6410ad3979ee61fa1b492b810dbf
toryoto <s1f102002345@iniad.org>
2024-07-03 12:57:18 +0900

    ブログ詳細から編集へのボタン

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,6 +1,7 @@
 import DeleteButton from '@/app/components/DeleteButton';
 import { getDetailArticle } from '@/blogAPI';
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
 const Article = async ({params}: {params: {id: string}}) => {
@@ -35,8 +36,19 @@ const Article = async ({params}: {params: {id: string}}) => {
               {detailArticle.content}
             </p>
           </div>
-          <div className='mt-8 flex justify-end'>
-            <DeleteButton id={detailArticle.id}/>
+          <div className='mt-12 space-y-4 sm:space-y-0 sm:flex sm:justify-between sm:items-center'>
+            <div className='flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2'>
+              <Link
+                href={`/articles/${detailArticle.id}/edit`}
+                className='inline-flex justify-center items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-300 ease-in-out w-full sm:w-auto'
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                </svg>
+                編集
+              </Link>
+              <DeleteButton id={detailArticle.id} />
+            </div>
           </div>
         </div>
       </div>
